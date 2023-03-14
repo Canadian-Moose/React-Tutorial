aecedcb1984514831b60478e32783620cedad4c8
Canadian-Moose <nrovelli@miners.utep.edu>
2023-03-13 20:09:30 -0600

    Lesson 8 Completed

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
+    //let welcome = "";
+    const [welcome, setWelcome] = useState("");
 
     const handleClick = (e) => {
-        console.log("Hello, Cyber Cats!", e);
+        setWelcome("Hello, Cyber Cat!");
     }
 
-    const handleClickAgain = (name) => {
+    /* const handleClickAgain = (name) => {
         console.log("Hello, " + name + "!");
-    }
+        <button onClick={() => handleClickAgain("Cyber Cat")}>Click This One Too!</button>
+    } */
 
     return (
         <div className="Home">
             <h2>Homepage</h2>
+            <p>{ welcome }</p>
             <button onClick={handleClick}>Click Me!</button>
-            <button onClick={() => {
-                handleClickAgain("Cyber Cat");
-            }}>Click This One Too!</button>
         </div>
     );
 }
