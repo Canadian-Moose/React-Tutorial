b97008fdf6580b8024a8749601932c3ca6a6b429
Canadian-Moose <nrovelli@miners.utep.edu>
2023-03-13 18:42:05 -0600

    Lesson 5 Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-    const title = "Welcome to the new blog!";
     const link = "http://google.com";
 
   return (
     <div className="App">
+        <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
           <p>Liked {Math.random() * 100} times.</p>
 
           <p>{"Hello, Cyber Cats"}</p>
