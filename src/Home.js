d0290cd8d8a9eda56f60a9aca953c4efce66d29f
Canadian-Moose <nrovelli@miners.utep.edu>
2023-03-15 18:50:54 -0600

    Lesson 12 Completed

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     //let welcome = "";
     const [welcome, setWelcome] = useState("");
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'pksp3', pin: true, id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'pksp3', pin: true, id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'pksp3', pin: false, id: 3 }
+    ])
 
     const handleClick = (e) => {
         setWelcome("Hello, Cyber Cat!");
@@ -14,10 +21,18 @@ const Home = () => {
     } */
 
     return (
-        <div className="Home">
+        <div className="home">
             <h2>Homepage</h2>
+            <br/>
             <p>{ welcome }</p>
+            <br/>
             <button onClick={handleClick}>Click Me!</button>
+            <br/>
+            <br/>
+            <BlogList blogs={blogs.filter(blog => blog.pin === true)} title="Pinned Blogs!" />
+            <br/>
+            <BlogList blogs={blogs} title="All Blogs" />
+            <br/>
         </div>
     );
 }
